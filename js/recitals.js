a755e4953328c9e0c39ad5e6258b35a65e6cec59
Ejay Sarangay <73544728+ejaysrngy@users.noreply.github.com>
2021-11-04 10:54:55 +0800

    changed animations on "Show Images"

--- js/recitals.js
+++ js/recitals.js
@@ -1,17 +1,28 @@
-function row1() {
-    const i = document.getElementById("row1-img")
-    if (i.style.display === "flex") {
-        i.style.display = 'none'
-    } else {
-        i.style.display = 'flex'
-    }
-}
+$("#row1-img").hide();
+$(".btn1").on("click", function(){
+   $("#row1-img").slideToggle();
+});
 
-function row2() {
-    const i = document.getElementById("row2-img")
-    if (i.style.display === "flex") {
-        i.style.display = 'none'
-    } else {
-        i.style.display = 'flex'
-    }
-}
+$("row2-img").hide();
+$("#row2-img").hide();
+$(".btn2").on("click", function(){
+   $("#row2-img").slideToggle();
+});
+
+// function row1() {
+//     const i = document.getElementById("row1-img")
+//     if (i.style.display === "flex") {
+//         i.style.display = 'none'
+//     } else {
+//         i.style.display = 'flex'
+//     }
+// }
+
+// function row2() {
+//     const i = document.getElementById("row2-img")
+//     if (i.style.display === "flex") {
+//         i.style.display = 'none'
+//     } else {
+//         i.style.display = 'flex'
+//     }
+// }
